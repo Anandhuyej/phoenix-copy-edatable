9df039b62b33555b592a27c75b092711d2e06a79
Anandhuyej <88794006+Anandhuyej@users.noreply.github.com>
2022-05-01 11:00:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -148,10 +148,10 @@ async def advantage_spoll_choker(bot, query):
         files, offset, total_results = await get_search_results(movie, offset=0, filter=True)
         if files:
             k = (movie, files, offset, total_results)
-            await auto_filter(bot, query, k, h)
+            await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit("<b><i>Movie Not available Reason\n\n1)O.T.T Or DVD Not Released\n\n2)Type Name With Year\n\n3)Movie Is Not Available in the database contact Admin</i></b>")
-            await asyncio.sleep(20)
+            k = await query.message.edit("<b><i>Movie Not available Reason\n\n1)O.T.T Or DVD Not Released\n\n2)Type Name With Year\n\n3)Movie Is Not Available in the database Request to Admins\n\nMovie Request Format:-\n#Request Moviename Year\n\nSeries Request Format:-\n#Request Seriesname Year</i></b>")
+            await asyncio.sleep(35)
             await k.delete()
  
 @Client.on_callback_query()
